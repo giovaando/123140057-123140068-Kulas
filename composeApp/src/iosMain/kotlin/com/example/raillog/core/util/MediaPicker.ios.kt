5dca7ca701c460fa4bfddd2650ea6e1ed37c516f
02-068-Giovan Lado <giovan.123140068@student.itera.ac.id>
2026-06-01 02:27:33 +0700

    feat: implement iOS-specific MediaPicker logic

--- composeApp/src/iosMain/kotlin/com/example/raillog/core/util/MediaPicker.ios.kt
+++ composeApp/src/iosMain/kotlin/com/example/raillog/core/util/MediaPicker.ios.kt
@@ -1,2 +1,14 @@
 package com.example.raillog.core.util
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+
+@Composable
+actual fun rememberMediaPicker(onMediaPicked: (String) -> Unit): MediaPicker {
+    return remember {
+        object : MediaPicker {
+            override fun launchCamera() { onMediaPicked("iOS_Camera_Simulated.jpg") }
+            override fun launchGallery() { onMediaPicked("iOS_Gallery_Simulated.pdf") }
+        }
+    }
+}
