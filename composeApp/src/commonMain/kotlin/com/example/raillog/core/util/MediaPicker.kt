3c05825c7cd366c5236a665d9a8e4a6fa2b1d387
02-068-Giovan Lado <giovan.123140068@student.itera.ac.id>
2026-06-01 02:27:07 +0700

    feat: create common MediaPicker interface for cross-platform support

--- composeApp/src/commonMain/kotlin/com/example/raillog/core/util/MediaPicker.kt
+++ composeApp/src/commonMain/kotlin/com/example/raillog/core/util/MediaPicker.kt
@@ -1,2 +1,11 @@
 package com.example.raillog.core.util
 
+import androidx.compose.runtime.Composable
+
+interface MediaPicker {
+    fun launchCamera()
+    fun launchGallery()
+}
+
+@Composable
+expect fun rememberMediaPicker(onMediaPicked: (String) -> Unit): MediaPicker
